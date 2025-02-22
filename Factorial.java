f33355a870ed1e98c18f02fa1a6f1092a2ae0944
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-02-22 23:08:32 +0530

    Create Factorial

---
+++ Factorial.java
@@ -0,0 +1,17 @@
+// How can you find the factorial of an integer in Java?
+
+import java.util.Scanner;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int fact = 1;
+		for(int i=num; i>0; i--) {
+			fact *= i;
+		}
+		System.out.println(fact);
+	}
+
+}
